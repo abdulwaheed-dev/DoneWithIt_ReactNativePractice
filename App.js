08e6377a8ca74c16aee54c70f87bd33db1226c5b
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-10-12 16:07:34 +0500

    basics-covered

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("text pressed");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Hello there, I am Abdul Waheed!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello there, I am Abdul Waheed!</Text>
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      <TouchableNativeFeedback onPress={() => console.log("view pressed.")}>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+        ></View>
+      </TouchableNativeFeedback>
+      <TouchableOpacity onPress={() => console.log("Image pressed.")}>
+        <Image
+          blurRadius={0}
+          fadeDuration={1000}
+          source={{
+            width: 320,
+            height: 240,
+            uri: "https://loremflickr.com/320/240",
+          }}
+        />
+      </TouchableOpacity>
+      <Button
+        color="red"
+        title="Click-Alert"
+        // onPress={() => alert("Butoon Tapped")}
+        onPress={() =>
+          Alert.alert("my title", "my message", [
+            { text: "Yes", onPress: () => console.log("yes") },
+            { text: "No", onPress: () => console.log("no") },
+          ])
+        }
+      />
+      {/* only work on ios. does not work on android, maybe work on anroid in future. */}
+      <Button
+        color="red"
+        title="Click-Prompt"
+        // onPress={() => alert("Butoon Tapped")}
+        onPress={() =>
+          Alert.prompt("my title", "my message", (text) => console.log(text))
+        }
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +65,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
