1799f66d5cfcf867490961e9219b461c980931b2
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-10-17 14:46:24 +0500

    DeviceOrientation

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import {
   SafeAreaView,
   StyleSheet,
   View,
   Platform,
   StatusBar,
   Dimensions,
+  Image,
 } from "react-native";
 
 export default function App() {
-  console.log(Dimensions.get("screen"));
+  //console.log(useDeviceOrientation());
+  const { landscape } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
       <View
-        style={{ width: "100%", height: "30%", backgroundColor: "dodgerblue" }}
+        style={{
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+          backgroundColor: "dodgerblue",
+        }}
       ></View>
     </SafeAreaView>
   );
